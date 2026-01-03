99a068bc7edfaf4fdcf6500c8d2043c2634415de
Giuseppe Bisicchia <giuseppe.bisicchia@phd.unipi.it>
2026-01-03 13:48:22 +0100

    Add Apache licensing and reproducibility guidance

--- decompress.sh
+++ decompress.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# Copyright 2024 Shot-wise contributors
+# Licensed under the Apache License, Version 2.0. See the LICENSE file for details.
+
 for file in $(find . -name "*.csv.gz"); do
     # Decompress the file in place
-    gunzip -f $file
+    gunzip -f "$file"
 done
 
 for file in $(find . -name "*.json.gz"); do
     # Decompress the file in place
-    gunzip -f $file
+    gunzip -f "$file"
 done
